c53d3fec8f498d807511525663c95a2bba5e8014
Elena Zhuvak <elenkazh@gmail.com>
2025-03-24 22:25:17 +0000

    adds function oneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+    const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    contacts.push(createFakeContact());
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+};
 
 addOneContact();
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,4 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
 export const readContacts = async () => {
+    const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    return contacts;
 };
