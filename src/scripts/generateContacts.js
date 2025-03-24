6507bc4760525c7dbffa35aca77b05c0b48e0b56
Elena Zhuvak <elenkazh@gmail.com>
2025-03-24 21:50:36 +0000

    adds generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { PATH_DB } from "../constants/contacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import fs from 'node:fs/promises';
+
+const generateContacts = async (number) => {
+    const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    for (let i = 0; i < number; i++) {
+        contacts.push(createFakeContact());
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
 
 generateContacts(5);
