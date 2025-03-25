8b61ef56043c2df688bc65cfea06661100135c42
Elena Zhuvak <elenkazh@gmail.com>
2025-03-25 23:16:59 +0000

    adds function removeLastContacts

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,11 @@
-export const removeLastContact = async () => {};
+import { PATH_DB } from "../constants/contacts.js";
+import * as fs from 'node:fs/promises';
+import { readContacts } from "../utils/readContacts.js";
+
+export const removeLastContact = async () => {
+    const contacts = await readContacts();
+    contacts.pop();
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+};
 
 removeLastContact();
